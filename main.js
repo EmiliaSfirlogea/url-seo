d3c652130bf33deadb7d25c1426f82ee9e03a573
Emilia Sfirlogea <madalina.sfirlogea@gmail.com>
2023-11-16 13:36:37 +0200

    fix empy values

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ new class {
         this.main()
     }
     getUrlData() {
-        return window.location.href.split("/url-seo/")[1].split('/')
-
+        return window.location.href.split("/url-seo/")[1].split('/').reduce((a, v) => {
+            if (v) a.push(v)
+            return a
+        }, [])
     }
     main() {
         console.log(this.getUrlData())
